4410575684857efbf0c51c0164601e344e44d2f5
GearShark123 <lylespurrell@hotmail.com>
2017-06-07 14:04:30 -0400

    Movie Class (Incomplete)
    Not sure about ToString() method.

--- Assignment_03_Theatre/Assignment_03_Theatre/Movie.cs
+++ Assignment_03_Theatre/Assignment_03_Theatre/Movie.cs
@@ -4,9 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[Flags]
+public enum MovieGenre
+{
+    Unrated = 0,
+    Action = 1,
+    Romance = 2,
+    Documentary = 4,
+    Mystery = 8,
+    Musical = 16,
+    Horror = 32,
+    Comedy = 64
+}
+
 namespace Assignment_03_Theatre
 {
     class Movie
     {
+        public int Length { get; private set; }
+        public string Title { get; private set; }
+        public MovieGenre Genre { get; private set; }
+        public List<string> Cast { get; private set; }
+
+        public Movie(string name, int length)
+        {
+            Cast = new List<string>();
+
+            this.Title = name;
+            this.Length = length;
+        }
+
+        public void AddActor(string actor)
+        {
+            Cast.Add(actor);
+        }
+
+        public void SetGenre(MovieGenre genre)
+        {
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(""); // Not sure about this.
+        }
     }
 }
