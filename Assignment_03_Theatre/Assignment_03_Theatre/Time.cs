20f3832bc134652b0f0be0696c31bed862d8d283
GearShark123 <lylespurrell@hotmail.com>
2017-06-07 13:21:11 -0400

    Time Class

--- Assignment_03_Theatre/Assignment_03_Theatre/Time.cs
+++ Assignment_03_Theatre/Assignment_03_Theatre/Time.cs
@@ -8,5 +8,21 @@ namespace Assignment_03_Theatre
 {
     class Time
     {
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+
+        public Time(int hours, int minutes, int seconds)
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+            this.Seconds = seconds;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}:{2}",
+                Hours, Minutes, Seconds);
+        }
     }
 }
