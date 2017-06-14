5e13b8b66cef2689ace05d78083ae4731cb8cfbe
Tum <tummytum3@gmail.com>
2017-06-13 21:28:18 -0400

    Finish other PrintShows() functions

--- Assignment_03_Theatre/Assignment_03_Theatre/Theatre.cs
+++ Assignment_03_Theatre/Assignment_03_Theatre/Theatre.cs
@@ -31,17 +31,29 @@ namespace Assignment_03_Theatre
 
         public void PrintShows(MovieGenre genre)
         {
-
+            foreach (Show s in Shows)
+            {
+                if (s.Movie.Genre == genre)
+                    Console.WriteLine(s);
+            }
         }
 
         public void PrintShows(MovieDay day)
         {
-
+            foreach (Show s in Shows)
+            {
+                if (s.Day == day)
+                    Console.WriteLine(s);
+            }
         }
 
         public void PrintShows(Time time)
         {
-
+            foreach (Show s in Shows)
+            {
+                if (s.Time == time)
+                    Console.WriteLine(s);
+            }
         }
     }
 }
